b5a3ec01fa35fe99ec7d33661370a26c80ba1472
Lucas Tavares <lucastvspvh90@gmail.com>
2023-02-13 11:48:11 -0400

    Implementando Controller person

--- RestWhitASPNET5/RestWhitASPNET5/Controllers/PersonController.cs
+++ RestWhitASPNET5/RestWhitASPNET5/Controllers/PersonController.cs
@@ -1,12 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWhitASPNET5.Models;
+using RestWhitASPNET5.Services.Implementations;
 
 namespace RestWhitASPNET5.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class PersonController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<PersonController> _logger;
+        private readonly IPersonService _personService;
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
-            return View();
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null)
+                NotFound();
+
+            return Ok(person);
+        }
+        
+        [HttpPost("{id}")]
+        public IActionResult Post([FromBody] Person person)
+        {          
+            if (person == null)
+                BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null)
+                BadRequest();
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+
+            return NoContent();
         }
     }
 }
+
---
+++ RestWhitASPNET5/RestWhitASPNET5/Services/Implementations/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWhitASPNET5.Models;
+using System.Collections.Generic;
+
+namespace RestWhitASPNET5.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long Id);
+        List<Person> FindAll();
+        Person Update(Person person);     
+        void Delete(long Id);
+    }
+}
