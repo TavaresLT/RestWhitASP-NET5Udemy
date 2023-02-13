7b6b79dc6519bda10efee38454cc8aa0180d0402
Lucas Tavares <lucastvspvh90@gmail.com>
2023-02-13 11:17:05 -0400

    implementado models

---
+++ RestWhitASPNET5/RestWhitASPNET5/Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWhitASPNET5.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
