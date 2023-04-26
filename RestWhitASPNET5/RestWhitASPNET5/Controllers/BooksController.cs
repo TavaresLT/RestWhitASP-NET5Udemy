dc78b98abfbea059787b85ae07284abd26ba97eb
Lucas Tavares <lucastvspvh90@gmail.com>
2023-04-26 13:32:43 -0400

    criando a controller de livros

---
+++ RestWhitASPNET5/RestWhitASPNET5/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWhitASPNET5.Business;
+using RestWhitASPNET5.Models;
+
+namespace RestWhitASPNET5.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{vaersion:apiVersion}")]
+    public class BooksController : Controller
+    {
+        private readonly ILogger<BooksController> logger;
+        private readonly IBooksBusiness booksBusiness;
+
+        public BooksController(ILogger<BooksController> logger, IBooksBusiness booksBusiness)
+        {
+            this.logger = logger;
+            this.booksBusiness = booksBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(booksBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var books = booksBusiness.FindById(id);
+            if (books == null)
+                NotFound();
+
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Books books)
+        {
+            if (books == null)
+                BadRequest();
+
+            return Ok(booksBusiness.Create(books));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Books books)
+        {
+            if (books == null)
+                BadRequest();
+
+            return Ok(booksBusiness.Update(books));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id) 
+        {
+            booksBusiness.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
