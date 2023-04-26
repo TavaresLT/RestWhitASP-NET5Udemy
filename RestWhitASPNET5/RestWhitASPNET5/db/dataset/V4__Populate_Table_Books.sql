1f41df96286b4b1cc2290637448ff5d2fd9d47b7
Lucas Tavares <lucastvspvh90@gmail.com>
2023-04-26 13:31:28 -0400

    Adicionando migrations ao sistema

---
+++ RestWhitASPNET5/RestWhitASPNET5/db/dataset/V4__Populate_Table_Books.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `books` (`id`, `title`, `author`, `launch_date`, `price`) VALUES
+	(1, 'Código limpo: Habilidades práticas do Agile Software', 'Marcelo D2', '2020/08/20 13:50:05', 48.49),
+	(2, 'Arte Da Guerra', 'Shin Tzu', '2020/08/20 13:50:05', 101.00),
+	(3, 'As 48 Leis do Poder', 'Robert Greene', '2020/08/20 13:50:05', 99.90),
+	(4, 'Crônicas de Gelo e Fogo', 'Albert Nilton', '2020/08/20 13:50:05', 48.00),
+	(5, 'Outsider', 'Stephen King', '2020/08/20 13:50:05', 48.00);
